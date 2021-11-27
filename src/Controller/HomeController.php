11b6140cb52440faa88a04f108c72d6b858de490
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2021-11-27 15:55:00 -0500

    Added home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function __invoke(): Response
+    {
+        return new Response('Hello');
+    }
+}
