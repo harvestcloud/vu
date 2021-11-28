b86467e6c0e359bb9794e88eae1c22a6df463a0f
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2021-11-28 18:11:19 -0500

    Added basic template

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function __invoke(): Response
     {
-        return new Response('Hello, attributes!');
+        return $this->render('home.html.twig');
     }
 }
