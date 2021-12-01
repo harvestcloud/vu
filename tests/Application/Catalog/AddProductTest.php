476103551cb8907e93ab058634840c447d76eff3
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2021-12-01 09:18:47 -0500

    Fix failing test

--- tests/Application/Catalog/AddProductTest.php
+++ tests/Application/Catalog/AddProductTest.php
@@ -9,8 +9,6 @@ class AddProductTest extends TestCase
 {
     public function test_can_be_created(): void
     {
-        $this->assertTrue(false);
-
         $addProduct = AddProduct::with(
             'b0663660-7323-42f6-b9a1-0ee6dc43e845',
             'My first product'
